bb3637e45e53f32ada3b4cd26ea432c82920ab4d
v.volkov <dvvwvert@gmail.com>
2019-11-14 09:31:53 +0200

    OMSKD-61

--- ssh/send_mail_after_30_day.php
+++ ssh/send_mail_after_30_day.php
@@ -32,7 +32,7 @@ global $USER;
 @set_time_limit(0);
 @ignore_user_abort(true);
 
- код сброса буфферов битрикса
+// код сброса буфферов битрикса
 ob_end_clean();
 ob_end_clean();
 ob_end_clean();
@@ -66,7 +66,7 @@ while ($Section = $section->GetNext()) {
             $time = strtotime($newDate);
             $Difference_time = (int)$_SERVER["REQUEST_TIME"] - (int)$time;
 
-            if($Difference_time > 2){    //больше чем 30 дней 2592000
+            if($Difference_time > 2592000){    //больше чем 30 дней 2592000
               $url = "devdoc1.kdteam.su/feedback/index.php";
               $arEventFields = array(
                 "MAIL" => $arUser["EMAIL"],
