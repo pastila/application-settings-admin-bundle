02ccabf99e1f1eb31bc8c24328073b8279e31612
a.fedorenko <a.fedorenko@kdteam.su>
2019-11-21 10:13:25 +0200

    OMSKD-71

--- ajax/img_delete.php
+++ ajax/img_delete.php
@@ -2,7 +2,7 @@
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
 $ID =  explode("_", $_POST['ID']);
-if ($ID[3] === 1) {
+if ($ID[3] === "1") {
     if ($ID[1]) {
         if (CModule::IncludeModule("iblock")) {
             $el = new CIBlockElement;
