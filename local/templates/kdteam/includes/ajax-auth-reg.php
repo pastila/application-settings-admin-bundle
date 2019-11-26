220d97209be640b9ad6a060e74db14dc5a1b6337
a.fedorenko <a.fedorenko@kdteam.su>
2019-11-26 11:24:42 +0200

    OMSKD-90

--- local/templates/kdteam/includes/ajax-auth-reg.php
+++ local/templates/kdteam/includes/ajax-auth-reg.php
@@ -34,7 +34,8 @@ CModule::IncludeModule("iblock");
             </div>
             <div class="input__wrap">
                 <label class="input__wrap_label">Ваш номер телефона</label>
-                <input id="phone" type="tel" minlength="16" name="phone" placeholder="+7(000)000-00-00"  required>
+                <input id="phone" type="tel" minlength="16" name="phone" data-mask="+7 (000) 000 00 00"
+                       placeholder="+7 (___) ___ __ __"  required>
             </div>
         </div>
         <div class="popup__wrap_middle">
