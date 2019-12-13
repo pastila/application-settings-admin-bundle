40293f9022dc73cc8aacf917d5d82066be4c5412
v.berezin <victor.berezin98@gmail.com>
2019-12-13 17:00:24 +0200

    OMSKD-123

---
+++ news/index.php
@@ -0,0 +1,161 @@
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+global $USER;
+
+use Bitrix\Main\Page\Asset;
+
+$asset = Asset::getInstance();
+$asset->addCss(SITE_TEMPLATE_PATH . "/pages/news/news.min.css");
+$asset->addJs(SITE_TEMPLATE_PATH . "/pages/news/news.min.js");
+?>
+    <ul class="breadcrumbs">
+        <li><a href="/">Главная</a></li>
+        <li>Читать про ОМС</li>
+    </ul>
+    <h1 class="page-title">Читать про ОМС</h1>
+
+    <div class="news_container">
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+        <div class="news_item white_block">
+            <a class="block_image" href="#">
+                <img src="https://yt3.ggpht.com/a/AGF-l787wN9RukUSOAR_8fAit4P_uYYDTAXsIoLUIw=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+            </a>
+            <div class="top_label">
+                <a href="#">Название новости мелкое</a>
+            </div>
+            <div class="name_news">
+                <a href="#">Название строки в несколько строчек возможно даже в три Название строки в несколько строчек возможно даже в три</a>
+            </div>
+            <div class="date_news">13.12.2019</div>
+            <a href="#" class="readmore_news">Продолжить чтение</a>
+        </div>
+
+    </div>
+
+
+<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
