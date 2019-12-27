82f10374f2afd4d1979914e4e198ffcad2156dd5
v.berezin <victor.berezin98@gmail.com>
2019-12-27 13:03:43 +0200

    OMSKD-130

--- forma-obrashenija/index.php
+++ forma-obrashenija/index.php
@@ -213,6 +213,35 @@ while($ob = $res->GetNextElement()){
 
                 <a class="link-underline" href="#">Ссылка на статью в блоге</a>
 
+                <div class="smart_search-block">
+                    <p class="title-select bold">Начните вводить наименование заболевания, с которым вы обратились в медицинскую организацию.</p>
+                    <form action="">
+                        <div class="input-with-search">
+                            <label class="title-select" for="user_pass">Выберите диагноз: </label>
+                            <div class="input__wrap">
+                                <div class="input__ico">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="255" height="255" viewBox="0 0 255 255"><path d="M0 63.75l127.5 127.5L255 63.75z"/></svg>
+                                </div>
+                                <input data-id_diagnoz="" id="diagnoz_input" type="text"
+                                       placeholder="Поиск по диагнозу" autocomplete="off" value=""/>
+                                <ul style="cursor: pointer;" class="custom-serach__items" id="search_result_diagnoz">
+                                    <li value="" id="empty_diagnoz" class="custom-serach__items_item diagnoz-js">Здесь нет моего диагноза</li>
+                                    <?php foreach ($arFields as $arItem) {?>
+                                        <li value="<?=$arItem['ID']?>" class="custom-serach__items_item  diagnoz-js"><?php echo $arItem['NAME']?></li>
+                                    <?php }?>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="wrap-chrckbox checkbox_registration">
+                            <label class="check-label">
+                                Я не знаю своего диагноза
+                                <input type="checkbox" value="">
+                                <span class="check-img"></span>
+                            </label></div>
+                    </form>
+                </div>
+
+                <p class="title-select bold">Или найдите диагноз в перечне заболеваний, структурированном в соответствии с международной классификацией болезней (МКБ-10).</p>
                 <div id="grid" class="grid" action="">
                     <?php
                     $APPLICATION->IncludeComponent(
