ed5b2e268ad07d1287b5e5570bb7a5b0fd733027
v.berezin <victor.berezin98@gmail.com>
2020-01-03 09:34:53 +0200

    OMSKD-152

--- local/templates/kdteam/footer.php
+++ local/templates/kdteam/footer.php
@@ -5,51 +5,37 @@ if (defined("B_PROLOG_INCLUDED") && B_PROLOG_INCLUDED === true) {
 ?>
 </main>
 
-<?php if ($APPLICATION->GetCurDir() != "/") { ?>
-    <!-- Footer -->
-    <footer class="footer">
-        <div class="footer__container">
-            <div class="footer__logo">
-                <img src="/local/templates/kdteam/images/svg/header/logo/logo_header.svg" alt="OMS">
+<!-- Footer -->
+<footer class="footer">
+    <div class="footer__container">
+        <div class="footer__logo">
+            <img src="/local/templates/kdteam/images/svg/header/logo/logo_header.svg" alt="OMS">
 
-                <div class="footer__logo_name">
-                    Company Name
-                </div>
+            <div class="footer__logo_name">
+                Company Name
             </div>
-
-            <ul class="footer__menu">
-                <li>
-                    <a href="#">Страховым организациям</a>
-                </li>
-                <li>
-                    <a href="#">О нас</a>
-                </li>
-                <li>
-                    <a href="#">связаться</a>
-                </li>
-                <li>
-                    <a href="/terms-of-use/">Пользовательское соглашение</a>
-                </li>
-                <li>
-                    <a href="/personal-data-processing/">Политика по обработке персональных данных</a>
-                </li>
-            </ul>
-
-            <!-- Custom Form -->
-
-            <form class="footer__form" action="">
-                <div class="custom-serach">
-                    <input type="text" name="search" placeholder="Поиск">
-
-                    <button class="search-btn">
-                        <img src="/local/templates/kdteam/images/svg/search.svg" alt="">
-                    </button>
-                </div>
-            </form>
-
         </div>
-    </footer>
-<?php } ?>
+
+        <ul class="footer__menu">
+            <li>
+                <a href="#">Страховым организациям</a>
+            </li>
+            <li>
+                <a href="#">О нас</a>
+            </li>
+            <li>
+                <a href="#">связаться</a>
+            </li>
+            <li>
+                <a href="/terms-of-use/">Пользовательское соглашение</a>
+            </li>
+            <li>
+                <a href="/personal-data-processing/">Политика по обработке персональных данных</a>
+            </li>
+        </ul>
+
+    </div>
+</footer>
 
 
 </div><!-- END Wrap -->
