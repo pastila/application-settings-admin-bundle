fa832bf64e381546cbc67af35dc2adab2a6a5af1
v.penkov <kdteamdev@KDTeam-iMac.local>
2020-01-13 14:43:51 +0200

    1

--- index.php
+++ index.php
@@ -21,51 +21,65 @@ CModule::IncludeModule("iblock");
        $arProps = $ob->GetProperties();
 
 ?>
-    <!-- Slider -->
-    <div class="slider">
-        <div class="slider__l">
-            <h1 class="home-title"><?=$arFields["NAME"]?></h1>
-            <div class="home-title-sub">
-                <?=$arFields["PREVIEW_TEXT"]?>
-            </div>
-
-            <div class="slider__r_descr">
-               <?=$arFields["DETAIL_TEXT"]?>
-            </div>
+<!-- Slider -->
+<div class="slider">
+    <div class="slider__l">
+        <h1 class="home-title"><?=$arFields["NAME"]?></h1>
+        <div class="home-title-sub">
+            <?=$arFields["PREVIEW_TEXT"]?>
         </div>
-    </div>
-<?}?>
-    <!-- Block buttons links to pages -->
-    <div class="buttons__items">
-        <!-- Item -->
-        <a href="/forma-obrashenija/" class="buttons__items_item">
-            <img class="buttons__items_item_img"
-                 src="./local/templates/kdteam/images/jpg/home/buttons_menu/item1/img.jpg" alt="">
 
+        <div class="slider__l_descr">
+            <?=$arFields["DETAIL_TEXT"]?>
+        </div>
+
+        <a href="/news/" class="slider__details mainBtn">Подробнее</a>
+    </div>
+</div>
+<?}?>
+<!-- Block buttons links to pages -->
+<div class="buttons__items">
+    <!-- Item -->
+    <a href="/forma-obrashenija/" class="buttons__items_item">
+        <div class="buttons__items_item_image-block">
+            <img class="buttons__items_item_img"
+                src="./local/templates/kdteam/images/jpg/home/buttons_menu/item1/img.jpg" alt="">
+        </div>
+
+        <div class="buttons__items_item_title-block">
             <h3 class="buttons__items_item_title">
                 Страховой случай в ОМС
             </h3>
-        </a>
+        </div>
+    </a>
 
-        <!-- Item -->
-        <a href="/feedback/" class="buttons__items_item">
+    <!-- Item -->
+    <a href="/feedback/" class="buttons__items_item">
+        <div class="buttons__items_item_image-block">
             <img class="buttons__items_item_img"
-                 src="./local/templates/kdteam/images/jpg/home/buttons_menu/item2/img.jpg" alt="">
+                src="./local/templates/kdteam/images/jpg/home/buttons_menu/item2/img.jpg" alt="">
+        </div>
 
+        <div class="buttons__items_item_title-block">
             <h3 class="buttons__items_item_title">
                 Отзывы о представителях
             </h3>
-        </a>
+        </div>
+    </a>
 
-        <!-- Item -->
-        <a href="/news/" class="buttons__items_item">
+    <!-- Item -->
+    <a href="/news/" class="buttons__items_item">
+        <div class="buttons__items_item_image-block">
             <img class="buttons__items_item_img"
-                 src="./local/templates/kdteam/images/jpg/home/buttons_menu/item3/img.jpg" alt="">
+                src="./local/templates/kdteam/images/jpg/home/buttons_menu/item3/img.jpg" alt="">
+        </div>
 
+        <div class="buttons__items_item_title-block">
             <h3 class="buttons__items_item_title">
                 Читать про ОМС
             </h3>
-        </a>
-    </div>
+        </div>
+    </a>
+</div>
 
 <?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
