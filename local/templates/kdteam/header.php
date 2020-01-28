305c874043f877076629375e16a630183d739379
v.penkov <kdteamdev@KDTeam-iMac.local>
2020-01-28 14:10:30 +0200

    1

--- local/templates/kdteam/header.php
+++ local/templates/kdteam/header.php
@@ -42,7 +42,6 @@ $url = $APPLICATION->GetCurDir();
 <!-- Start Wrap -->
 <div class="wrap"
     <?php if ($APPLICATION->GetCurDir() === '/') {?>
-        style="background-image: url('/local/templates/kdteam/images/jpg/home/main-image_2x.jpg')"
     <?php } ?>>
     <!-- Header -->
     <header class="header">
