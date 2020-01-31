b78bc9560067f940475dedee67fdcaeda2ed17ce
v.berezin <victor.berezin98@gmail.com>
2020-01-31 11:27:23 +0200

    OMSKD-228

--- feedback/comment.php
+++ feedback/comment.php
@@ -73,7 +73,7 @@ preg_match("/(\d+)\/$/",$url,$result_id);
             /* владик */
             $compani = CIBlockElement::GetByID($arProps["NAME_COMPANY"]["VALUE"])->GetNextElement()->GetProperties();
 
-            $file = CFile::ResizeImageGet($compani["LOGO_IMG"]["VALUE"], array('width' => 100, 'height' => 100),
+            $file = CFile::ResizeImageGet($compani["LOGO_IMG"]["VALUE"], array('width' => 120, 'height' => 80),
                 BX_RESIZE_IMAGE_PROPORTIONAL, true);
 
             ?>
