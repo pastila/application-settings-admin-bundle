4486fa089cc38ac172b8c9657b7fa71fc3eda339
v.volkov <dvvwvert@gmail.com>
2020-03-24 18:50:56 +0200

    BEZBAHILKD-41

--- gulp/js/main.js
+++ gulp/js/main.js
@@ -237,6 +237,7 @@ $(document).ready(function() {
 
           });
     });
+    console.log();
 
     $(document).on('click', '.hospital_reg', function() {
       let id_region = $(this).attr('value');
