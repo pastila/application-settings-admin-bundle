c369a4258bcfc27c27e8254e5ec795a7e0f93116
Александр Агапов <aagapov@accurateweb.ru>
2020-09-21 13:06:36 +0500

    [BEZBAHIL-52] fix composer and fpm files

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,18 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Accurateweb\ImagingBundle\AccuratewebImagingBundle(),
+            new Accurateweb\MediaBundle\AccuratewebMediaBundle(),
             new AppBundle\AppBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
@@ -48,14 +58,8 @@ class AppKernel extends Kernel
         return dirname(__DIR__).'/var/logs';
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration (LoaderInterface $loader)
     {
-        $loader->load(function (ContainerBuilder $container) {
-            $container->setParameter('container.autowiring.strict_mode', true);
-            $container->setParameter('container.dumper.inline_class_loader', true);
-
-            $container->addObjectResource($this);
-        });
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+      $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
