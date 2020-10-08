8c57e7e1076629ffcaf82af9337256d32b31cedd
Denis N. Ragozin <dragozin@accurateweb.ru>
2020-10-08 16:56:41 +0500

    *Исправлена ошибка верстки в личном кабинете

--- web/local/templates/kdteam/components/bitrix/news.list/add_child/template.php
+++ web/local/templates/kdteam/components/bitrix/news.list/add_child/template.php
@@ -116,7 +116,7 @@ $this->setFrameMode(true);
                     </div>
                     <div class="item_data">
                         <div class="logo_block">
-                            <img src="<?=$logo_company["SRC"]?>" alt="<?php htmlspecialchars($hospital); ?>">
+                            <img src="<?=$logo_company["SRC"]?>" alt="<?php echo htmlspecialchars($hospital); ?>">
                         </div>
                         <p><?= $hospital ?></p>
                     </div>
