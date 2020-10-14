eeb1e78514d813645a9d31f7d121a2e19f9a1881
Александр Агапов <aagapov@accurateweb.ru>
2020-10-14 16:21:08 +0500

    [BEZBAHIL-71]
    это "обратные" изменения по этой задаче, так как она уже в develop, только после обсуждения, примем решение, можно ли использовать названия филиалов для компаний

--- src/AppBundle/Helper/Feedback/CompanyHelper.php
+++ src/AppBundle/Helper/Feedback/CompanyHelper.php
@@ -49,31 +49,26 @@ class CompanyHelper
                     e.NAME, 
                     e.IBLOCK_ID, 
                     e.ACTIVE, 
-                    ANY_VALUE(epKpp.VALUE) as KPP,
-                    ANY_VALUE(epI.VALUE) as IMAGE,                    
-                    ANY_VALUE(epE1.VALUE) as EMAIL1,                    
-                    ANY_VALUE(epE2.VALUE) as EMAIL2,                    
-                    ANY_VALUE(epE2.VALUE) as EMAIL3,
-                    ANY_VALUE(ieB.NAME) as NAME_BRANCH
+                    epKpp.VALUE as KPP,
+                    epI.VALUE as IMAGE,                    
+                    epE1.VALUE as EMAIL1,                    
+                    epE2.VALUE as EMAIL2,                    
+                    epE2.VALUE as EMAIL3
             FROM b_iblock_element e
             LEFT JOIN b_iblock_element_property epKpp ON epKpp.IBLOCK_ELEMENT_ID = e.ID AND epKpp.IBLOCK_PROPERTY_ID = 144    
             LEFT JOIN b_iblock_element_property epI ON epI.IBLOCK_ELEMENT_ID = e.ID AND epI.IBLOCK_PROPERTY_ID = 139                
             LEFT JOIN b_iblock_element_property epE1 ON epE1.IBLOCK_ELEMENT_ID = e.ID AND epE1.IBLOCK_PROPERTY_ID = 135      
             LEFT JOIN b_iblock_element_property epE2 ON epE2.IBLOCK_ELEMENT_ID = e.ID AND epE2.IBLOCK_PROPERTY_ID = 136      
-            LEFT JOIN b_iblock_element_property epE3 ON epE3.IBLOCK_ELEMENT_ID = e.ID AND epE3.IBLOCK_PROPERTY_ID = 137   
-            LEFT JOIN b_iblock_element_property epK ON epK.VALUE = epKpp.VALUE AND epK.IBLOCK_PROPERTY_ID = 112
-            LEFT JOIN b_iblock_element ieB ON ieB.ID = epK.IBLOCK_ELEMENT_ID
-            WHERE e.IBLOCK_ID = 24 AND e.ACTIVE = "Y"
-            GROUP BY e.ID';
+            LEFT JOIN b_iblock_element_property epE3 ON epE3.IBLOCK_ELEMENT_ID = e.ID AND epE3.IBLOCK_PROPERTY_ID = 137                    
+            WHERE e.IBLOCK_ID = 24 AND e.ACTIVE = "Y"';
     $stmt = $conn->prepare($sql);
     $stmt->execute();
 
     $result = $stmt->fetchAll();
     $nbImported = 0;
-    foreach ($result as $item) {
-      $name = !empty($item['NAME_BRANCH']) ? $item['NAME_BRANCH'] : $item['NAME'];
-      $name = str_replace('"', '', $name);
-      $name = trim($name);
+    foreach ($result as $item)
+    {
+      $name = !empty($item['NAME']) ? str_replace('"', '', $item['NAME']) : null;
       $kpp = !empty($item['KPP']) ? $item['KPP'] : null;
       $image = !empty($item['IMAGE']) ? $item['IMAGE'] : null;
       $email1 = !empty($item['EMAIL1']) ? $item['EMAIL1'] : null;
@@ -109,14 +104,16 @@ class CompanyHelper
    */
   private function checkBranch()
   {
-    try {
+    try
+    {
       $sql = 'UPDATE s_company_branches scb 
               LEFT JOIN s_companies sc ON sc.id = scb.company_id
               SET scb.company_id = null
               WHERE scb.company_id IS NOT NULL AND sc.id IS NULL';
       $stmt = $this->entityManager->getConnection()->prepare($sql);
       $stmt->execute();
-    } catch (Throwable $exception) {
+    } catch (Throwable $exception)
+    {
       $this->logger->error(sprintf('Error update s_company_branches: . %s', $exception->getMessage()));
 
       throw $exception;
