25c0c8ecd9bb861a5f5071e4fb998aac83d9ad93
dragozin <dragozin@accurateweb.ru>
2020-10-26 19:03:02 +0500

    *minor fixes

--- ci/phpunit.sh
+++ ci/phpunit.sh
@@ -3,5 +3,5 @@
 php bin/console cache:clear --env=test
 php bin/console doctrine:database:drop --force --env=test
 php bin/console doctrine:database:create --env=test
-php bin/console doctrine:schema:update --env=test --no-interaction
+php bin/console doctrine:schema:update --force --env=test --no-interaction
 vendor/bin/phpunit --teamcity
