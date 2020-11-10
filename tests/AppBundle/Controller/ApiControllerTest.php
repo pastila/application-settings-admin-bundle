0c87facb90cb7f3b72f04f7e785675c4bb44734b
Александр Агапов <aagapov@accurateweb.ru>
2020-11-10 16:45:01 +0500

    [BEZBAHIL-84]
    + функ.тесты по задаче

--- tests/AppBundle/Controller/ApiControllerTest.php
+++ tests/AppBundle/Controller/ApiControllerTest.php
@@ -22,5 +22,26 @@ class ApiControllerTest extends AppWebTestCase
      */
     $client->request('GET', '/api/v1/regions');
     $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+    $res = $client->getResponse();
+    $content = !empty($res) ? $res->getContent() : null;
+    $content = !empty($content) ? json_decode($content, true) : null;
+
+    // Проверка, что данные не пустые
+    $this->assertTrue(is_array($content) && count($content) > 0);
+
+    /**
+     * Проверка, через поиск региона, который был добавлен первым,
+     * так как в последствии в тесты будут добавлены еще регионы и не можем сейчас проверять их кол-во
+     */
+    $isSearch = false;
+    foreach ($content['regions'] as $region)
+    {
+      if ($region['name'] === 'Свердловская область')
+      {
+        $isSearch = true;
+      }
+    }
+    $this->assertTrue($isSearch);
   }
 }
