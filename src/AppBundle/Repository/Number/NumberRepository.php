716a37f6f7d0be2fddcbabd78b8bab1e35638e7b
Александр Агапов <aagapov@accurateweb.ru>
2020-11-10 13:41:16 +0500

    [BEZBAHIL-90]
    * вывожу на фронт из базы "Цифры"

--- src/AppBundle/Repository/Number/NumberRepository.php
+++ src/AppBundle/Repository/Number/NumberRepository.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Repository\Number;
 
 use Gedmo\Sortable\Entity\Repository\SortableRepository;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Class NumberRepository
@@ -10,4 +11,14 @@ use Gedmo\Sortable\Entity\Repository\SortableRepository;
  */
 class NumberRepository extends SortableRepository
 {
+  /**
+   * @return QueryBuilder
+   */
+  public function getAll()
+  {
+    $qb = $this->createQueryBuilder('nm')
+      ->orderBy('nm.position', 'ASC');
+
+    return $qb;
+  }
 }
