3869f367cc0fb2c962954914e4145d34f964ca1b
Александр Агапов <aagapov@accurateweb.ru>
2020-11-25 13:55:23 +0500

    [BEZBAHIL-92]

---
+++ app/DoctrineMigrations/Version20201125084001.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201125084001 extends AbstractMigration
+{
+  /**
+   * @param Schema $schema
+   */
+  public function up(Schema $schema)
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    $this->addSql("INSERT INTO settings (name, value) VALUES ('agreement', null)");
+    $this->addSql("INSERT INTO settings (name, value) VALUES ('personal_data', null)");
+  }
+
+  /**
+   * @param Schema $schema
+   */
+  public function down(Schema $schema)
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+    $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+    $this->addSql("DELETE FROM settings WHERE name='agreement'");
+    $this->addSql("DELETE FROM settings WHERE name='personal_data'");
+  }
+}
