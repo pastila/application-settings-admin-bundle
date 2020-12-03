6eeb1dd847422bc22d21bdaa2f27645b3d1f15c7
Александр Агапов <aagapov@accurateweb.ru>
2020-12-03 20:42:52 +0500

    [BEZBAHIL-84]
    - удалил api получение региона
    + отображние актуального списка регионов в модалке
    * изменил фун.тесты

--- deploy.php
+++ deploy.php
@@ -215,6 +215,7 @@ set('rsync', array_merge(get('rsync'), [
         'web/local/templates/kdteam/js',
         'web/local/templates/kdteam/styles',
         'web/local/templates/kdteam/pages',
+        'web/dist'
     ],
     'options' => [] //No delete
 ]));
---
+++ web/regionSelectionModal/regionSelectionModal.js
@@ -0,0 +1,32 @@
+export default function initRegionSelectionModal() {
+    const locationWindow = $('[data-remodal-id=loacation]').remodal();
+    $('.js-loacation').on('click', function (event) {
+        event.preventDefault();
+        if (locationWindow) {
+            locationWindow.open();
+        }
+    });
+    const regionModalError = $('[data-remodal-id=error-modal]').remodal();
+    $('.l-list__item a').on('click', function (event) {
+        event.preventDefault();
+        if (locationWindow) {
+            locationWindow.close();
+        }
+        $.get(`${urlPrefix}/select-location`, {region_id: event.target.dataset.region})
+            .done((data) => {
+                if (data === 1) {
+                    $('.js-location').html($(this).html());
+                    window.location.reload();
+                } else {
+                    if (regionModalError) {
+                        regionModalError.open();
+                    }
+                }
+            })
+            .fail(() => {
+                if (regionModalError) {
+                    regionModalError.open();
+                }
+            })
+    });
+}
