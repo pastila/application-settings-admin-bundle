5c76d6b2a3072af4483f264fef697bbddcde7ec3
Александр Агапов <aagapov@accurateweb.ru>
2020-12-18 18:22:47 +0500

    [BEZBAHIL-129]

--- web/ajax/sms_code_generate.php
+++ web/ajax/sms_code_generate.php
@@ -12,8 +12,22 @@ if ($arSpecUser) {
     echo 'error';
 } else {
     session_start();
+    $currentTime = new \DateTime();
+    if (!empty($_SESSION['SMS_DATETIME']))
+    {
+      /**
+       * Если в сессии есть запись, о уже отправленной смс
+       * Проверям, что она была отправлена не раньше 30 сек
+       */
+      $sessionTime = new \DateTime($_SESSION['SMS_DATETIME']);
+      $sessionTime->add(new DateInterval('PT30S'));
+      if ($sessionTime > $currentTime)
+      {
+        http_response_code(429);
+        exit;
+      }
+    }
     $apiSms = new \Kdteam\ApiSms();
-
     $permitted_chars = '0123456789';
 
     function generate_string($input, $strength) {
@@ -29,6 +43,7 @@ if ($arSpecUser) {
     $sms_code = generate_string($permitted_chars, 5);
 
     $_SESSION['SMS_CODE'] = $sms_code;
+    $_SESSION['SMS_DATETIME'] = $currentTime->format('Y-m-d H:i:s');
 
     $result = $apiSms->execCommad('registerSender', array('name' => "bezbahil", 'country' => "ru"));
     $result = $apiSms->execCommad("sendSMS", array(
@@ -38,5 +53,4 @@ if ($arSpecUser) {
         'datetime' => "",
         'sms_lifetime' => "0"
     ));
-
 }
