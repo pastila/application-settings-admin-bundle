18e244ed7384d1e59ba550dcb3fa24cd1e27d9bc
Александр Агапов <aagapov@accurateweb.ru>
2020-12-23 14:25:27 +0500

    [BEZBAHIL-157]

--- src/AppBundle/Repository/Geo/RegionRepository.php
+++ src/AppBundle/Repository/Geo/RegionRepository.php
@@ -37,20 +37,4 @@ class RegionRepository extends ServiceEntityRepository implements UserLocationRe
       ->setMaxResults(1)
       ->getOneOrNullResult();
   }
-
-  /**
-   * @param $name
-   * @return \Doctrine\ORM\QueryBuilder
-   */
-  public function findByNameQueryBuilder($name)
-  {
-    $q = $this->createQueryBuilder('r');
-    if ($name)
-    {
-      return $q
-        ->andWhere('r.name LIKE :name')
-        ->setParameter('name', '%' . $name . '%');
-    }
-    return $q;
-  }
 }
