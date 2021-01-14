05dbda3147096a25102b4c2f05579baaff6acaf2
Александр Агапов <aagapov@accurateweb.ru>
2021-01-14 19:42:31 +0500

    [BEZBAHIL-186]

--- src/AppBundle/Admin/Company/CompanyAdmin.php
+++ src/AppBundle/Admin/Company/CompanyAdmin.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Admin\Company;
 
 use AppBundle\Entity\Company\CompanyBranch;
+use AppBundle\Entity\Company\CompanyStatus;
 use AppBundle\Entity\Company\FeedbackModerationStatus;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -11,6 +12,7 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\CoreBundle\Form\Type\DatePickerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\Length;
@@ -30,10 +32,10 @@ class CompanyAdmin extends AbstractAdmin
     $list
       ->add('id')
       ->add('name')
-      ->add('moderationStatus', 'choice', [
+      ->add('status', 'choice', [
         'label' => 'Статус модерации',
         'editable' => true,
-        'choices' => FeedbackModerationStatus::getAvailableNames(),
+        'choices' => CompanyStatus::getAvailableName(),
       ])
       ->add('_action', null, array(
         'label' => 'Действия',
@@ -50,20 +52,7 @@ class CompanyAdmin extends AbstractAdmin
   protected function configureFormFields(FormMapper $form)
   {
     $form
-      ->add('branch', EntityType::class, [
-        'label' => 'Филиал СМО',
-        'class' => CompanyBranch::class,
-        'choice_label' => 'regionName',
-      ])
-      ->add('author', null, [
-        'label' => 'Авторизованный пользователь',
-        'help' => 'Можно оставить пустым, если пользватель не авторизованный',
-      ])
-      ->add('authorName', null, [
-        'label' => 'Имя пользователя',
-        'help' => 'Подставляется автоматически после сохранения, если пользователь выбран как авторизованный',
-      ])
-      ->add("title", TextType::class, [
+      ->add("name", TextType::class, [
         'required' => true,
         'constraints' => [
           new Length([
@@ -72,41 +61,23 @@ class CompanyAdmin extends AbstractAdmin
           ]),
         ],
       ])
-      ->add("text", TextareaType::class, [
+      ->add("kpp", TextType::class, [
         'required' => true,
-        'attr' => [
-          'rows' => 5
-        ],
         'constraints' => [
           new Length([
-            'min' => 10,
+            'min' => 3,
+            'max' => 255,
           ]),
         ],
       ])
-      ->add('valuation', 'AppBundle\Form\Feedback\FeedbackValuationChoiceType', [
-        'label' => 'Оценка',
-        'required' => false,
-        'expanded' => false,
-      ])
-      ->add("createdAt", DatePickerType::class, [
-        'format' => 'd MMMM yyyy',
-        'view_timezone' => 'UTC',
-        'model_timezone' => 'UTC',
-      ])
-      ->add('moderationStatus', 'AppBundle\Form\Feedback\FeedbackStatusChoiceType', [
+      ->add("valuation", NumberType::class, [
         'required' => true,
-        'label' => 'Статус модерации',
+      ])
+      ->add('status', 'AppBundle\Form\Company\CompanyStatusChoiceType', [
+        'required' => true,
         'constraints' => [
           new NotBlank(),
         ],
       ]);
-  }
-
-  /**
-   * @param RouteCollection $collection
-   */
-  protected function configureRoutes(RouteCollection $collection)
-  {
-    $collection->remove('create');
   }
 }
