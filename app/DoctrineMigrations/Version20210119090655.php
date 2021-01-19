ee378543a57ae85f3e99ef4c786e80172383343b
Александр Агапов <aagapov@accurateweb.ru>
2021-01-19 14:16:36 +0500

    [BEZBAHIL-222]

---
+++ app/DoctrineMigrations/Version20210119090655.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210119090655 extends AbstractMigration
+{
+  public function up(Schema $schema): void
+  {
+    // this up() migration is auto-generated, please modify it to your needs
+
+    $this->addSql('UPDATE settings SET name = \'info_email\' WHERE name = \'main_email\'');
+  }
+
+  public function down(Schema $schema): void
+  {
+    // this down() migration is auto-generated, please modify it to your needs
+
+    $this->addSql('UPDATE settings SET name = \'main_email\' WHERE name = \'info_email\'');
+  }
+}
--- src/AppBundle/Controller/InsuranceCompany/FeedbackController.php
+++ src/AppBundle/Controller/InsuranceCompany/FeedbackController.php
@@ -298,11 +298,11 @@ class FeedbackController extends Controller
       $em->persist($feedback);
       $em->flush();
 
-      if (!empty($this->settingManager->getValue('main_email')))
+      if (!empty($this->settingManager->getValue('info_email')))
       {
         try
         {
-          $this->feedbackMailer->sendFeedback($feedback, $this->settingManager->getValue('main_email'));
+          $this->feedbackMailer->sendFeedback($feedback, $this->settingManager->getValue('info_email'));
         }
         catch (\Exception $e)
         {
