e83abe80fb7060876ab4e4eee07999f741f2be4e
Denis N. Ragozin <dragozin@accurateweb.ru>
2021-01-22 20:32:51 +0500

    *doctrine:database:drop добавлен --if-exists

--- ci/phpunit.sh
+++ ci/phpunit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 php bin/console cache:clear --env=test
-php bin/console doctrine:database:drop --force --env=test
+php bin/console doctrine:database:drop --if-exists --force --env=test
 php bin/console doctrine:database:create --env=test
 php bin/console doctrine:schema:update --force --env=test --no-interaction
 vendor/bin/phpunit --log-junit var/logs/phpunit-unit.xml
