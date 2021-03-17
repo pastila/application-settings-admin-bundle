63479a3372ee93a02dc704f5953ef632cd314558
Александр Агапов <aagapov@accurateweb.ru>
2021-03-17 17:21:32 +0500

    [BEZBAHIL-176]

--- src/AppBundle/EventListener/ResettingListener.php
+++ src/AppBundle/EventListener/ResettingListener.php
@@ -5,8 +5,11 @@ namespace AppBundle\EventListener;
 
 use FOS\UserBundle\Event\GetResponseUserEvent;
 use FOS\UserBundle\EventListener\ResettingListener as BaseResettingListener;
+use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use FOS\UserBundle\Event\FormEvent;
 
 /**
  * Class ResettingListener
@@ -25,17 +28,34 @@ class ResettingListener extends BaseResettingListener
   private $tokenTtl;
 
   /**
-   * ResettingListener constructor.
-   *
-   * @param UrlGeneratorInterface $router
-   * @param int                   $tokenTtl
+   * @var SessionInterface
    */
-  public function __construct(UrlGeneratorInterface $router, $tokenTtl)
+  private $session;
+
+  /**
+   * ResettingListener constructor.
+   * @param UrlGeneratorInterface $router
+   * @param $tokenTtl
+   * @param SessionInterface $session
+   */
+  public function __construct(UrlGeneratorInterface $router, $tokenTtl, SessionInterface $session)
   {
     $this->router = $router;
     $this->tokenTtl = $tokenTtl;
+    $this->session = $session;
 
     parent::__construct($router, $tokenTtl);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents()
+  {
+    return array_merge(parent::getSubscribedEvents(), [
+      FOSUserEvents::RESETTING_SEND_EMAIL_INITIALIZE => 'onResettingSendEmailInitialize',
+      FOSUserEvents::RESETTING_RESET_SUCCESS => 'onPasswordResettingSuccess',
+    ]);
   }
 
   /**
@@ -43,8 +63,32 @@ class ResettingListener extends BaseResettingListener
    */
   public function onResettingResetInitialize(GetResponseUserEvent $event)
   {
-    if (!$event->getUser()->isPasswordRequestNonExpired($this->tokenTtl)) {
-      $event->setResponse(new RedirectResponse($this->router->generate('fos_user_resetting_reference_outdated')));
+    if (!$event->getUser()->isPasswordRequestNonExpired($this->tokenTtl))
+    {
+      $this->session->getFlashBag()->add('errors', 'Ссылка устарела и больше не является действительной!');
+      $event->setResponse(new RedirectResponse($this->router->generate('fos_user_resetting_request')));
     }
+  }
+
+  /**
+   * @param GetResponseUserEvent $event
+   */
+  public function onResettingSendEmailInitialize(GetResponseUserEvent $event)
+  {
+    if (!$event->getUser())
+    {
+      $this->session->getFlashBag()->add('errors', 'Учетная запись с таким e-mail не найдена!');
+      $event->setResponse(new RedirectResponse($this->router->generate('fos_user_resetting_request')));
+    }
+  }
+
+  /**
+   * @param FormEvent $event
+   */
+  public function onPasswordResettingSuccess(FormEvent $event)
+  {
+    $url = $this->router->generate('cabinet');
+
+    $event->setResponse(new RedirectResponse($url));
   }
 }
